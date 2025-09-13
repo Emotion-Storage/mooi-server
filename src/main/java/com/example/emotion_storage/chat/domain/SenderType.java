3cc82c01686a4e233f18cfca122d3ca8c57db1fd
Minhyuk Kim <rhrlwld@gmail.com>
2025-09-13 21:54:58 +0900

    Revert "Merge branch 'main' into feat/home"
    This reverts commit e4b5195689abdd608318f83e259dec78a84c1167, reversing
    changes made to f1ff65197db1e0a505f341fdf8f8e928ddfb5e61.

--- src/main/java/com/example/emotion_storage/chat/domain/SenderType.java
+++
@@ -1,5 +0,0 @@
-package com.example.emotion_storage.chat.domain;
-
-public enum SenderType {
-    MOOI, USER
-}
--- src/main/java/com/example/emotion_storage/chat/dto/response/ChatRoomCloseResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.emotion_storage.chat.dto.response;
-
-public record ChatRoomCloseResponse(
-        boolean finished
-) {}
