844a095842b019a7c65822ab8936f44361c23512
JoonKyoLee <joonkyolee@gmail.com>
2026-02-19 13:45:54 +0900

    feat: 애플 로그인 지원을 위한 사용자 스키마 보완

--- src/main/java/com/example/emotion_storage/user/domain/User.java
+++ src/main/java/com/example/emotion_storage/user/domain/User.java
@@ -44,6 +44,10 @@ import org.hibernate.annotations.Where;
                 @UniqueConstraint(
                         name = "uk_users_email_deleted_at",
                         columnNames = {"email", "deleted_at"}
+                ),
+                @UniqueConstraint(
+                        name = "uk_users_social_deleted_at",
+                        columnNames = {"social_type", "social_id", "deleted_at"}
                 )
         }
 )
@@ -67,7 +71,6 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String socialId;
 
-    @Column(nullable = false)
     private String email;
 
     private String profileImageUrl;
