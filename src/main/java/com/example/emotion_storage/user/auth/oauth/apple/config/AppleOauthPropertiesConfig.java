7d0d0178a167c37451eaa0fef524d84a31ae6a51
JoonKyoLee <joonkyolee@gmail.com>
2026-02-19 12:49:26 +0900

    feat: Apple OAuth 설정 및 기본 구성 추가

---
+++ src/main/java/com/example/emotion_storage/user/auth/oauth/apple/config/AppleOauthPropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.example.emotion_storage.user.auth.oauth.apple.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AppleOauthProperties.class)
+public class AppleOauthPropertiesConfig {
+}
